a2376f37efb4fbaafc371202ec713541cce17b07
Sandip <Sandip>
2020-09-01 09:24:22 +0530

    Create a wraper funtion to check disk full

--- check_all.py
+++ check_all.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python3
 import os
 import sys
+import shutil
 
 def reboot_all():
-    return os.path.exist("/run/reboot required")
+    return os.path.exists("/run/reboot required")
+
+def check_disk_full(disk,min_gb,min_percent):
+    du=shutil.disk_usage(disk)
+    percent_free=100*du.free/du.total
+    gigabytes_free=du.free/2**30
+    if gigabytes_free<min_gb or percent_free<min_percent:
+        return True
+    return False
+
+
+def check_root_full():
+    return check_disk_full(disk="/",min_gb=2,min_percent=10)
 
 def main():
     if reboot_all():
         print("Pending reboot")
         sys.exit(1)
-    else:
-        print("Not needed reboot")
-        sys.exit(0)
+    if check_root_full():
+        print("Root Full!")
+        sys.exit(1)
+    print("Everything OK!")
+    sys.exit(0)
 
 
 main()
